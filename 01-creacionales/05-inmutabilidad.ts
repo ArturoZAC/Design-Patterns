6f8f560f99c8078fc4f0e521af198819f7245540
4z4c <arturoyz2105@gmail.com>
2025-06-12 10:36:25 -0500

    inmutabilidad

--- 01-creacionales/05-inmutabilidad.ts
+++ 01-creacionales/05-inmutabilidad.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors.ts';
 /**
  * ! Inmutabilidad con copia
  * Aunque la inmutabilidad es una buena práctica, no siempre es posible.
@@ -6,3 +7,68 @@
  *  * Es útil para mantener un historial de estados en aplicaciones interactivas.
  *
  */
+
+class CodeEditorState {
+
+  readonly content: string;
+  readonly cursorPosition: number;
+  readonly unsavedChanges: boolean;
+
+  constructor(
+    content: string,
+    cursorPosition: number,
+    unsavedChanges: boolean
+  ) {
+    this.content = content;
+    this.cursorPosition = cursorPosition;
+    this.unsavedChanges = unsavedChanges;
+  }
+
+  copyWith ({
+    content,
+    cursorPosition,
+    unsavedChanges
+  }: Partial<CodeEditorState>): CodeEditorState {
+    return new CodeEditorState(
+      content ?? this.content,
+      cursorPosition ?? this.cursorPosition,
+      unsavedChanges ?? this.unsavedChanges
+    )
+  }
+
+  displayState(): void {
+    console.log('\n%cEstado del editor: ', COLORS.green);
+    console.log(`
+      Contenido: ${this.content}
+      Cursor Post: ${this.cursorPosition}
+      Unsaved changes: ${this.unsavedChanges}      
+    `);
+  }
+  
+}
+
+class CodeEditorHistory {
+
+  private history: CodeEditorState[] = [];
+  private currentIndex: number = -1; // 0, 1 , 2 , 3, 4, 5, 6
+
+  save ( state: CodeEditorState ):void {
+
+    if ( this.currentIndex < this.history.length -1 ) {
+      this.history = this.history.splice( 0, this.currentIndex + 1 );
+    }
+
+    this.history.push(state);
+    this.currentIndex++;
+  }
+
+  redo (): CodeEditorState | null {
+    if ( this.currentIndex < this.history.length -1 ) {
+      this.currentIndex++;
+      return this.history[this.currentIndex];
+    }
+    
+    return null;
+  }
+
+}
