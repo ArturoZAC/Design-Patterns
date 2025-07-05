fdfd1723fe4436c6f731d20f71a62b094df2c774
4z4c <arturoyz2105@gmail.com>
2025-07-04 22:02:05 -0500

    addMemento

--- 03-comportamiento/05-memento.ts
+++ 03-comportamiento/05-memento.ts
@@ -15,50 +15,47 @@ class GameMemento {
   private healt: number;
   private position: string;
 
-  constructor(level: number, healt: number, position: string){
+  constructor(level: number, healt: number, position: string) {
     this.level = level;
     this.healt = healt;
     this.position = position;
   }
 
-  getLevel(){
+  getLevel() {
     return this.level;
   }
 
-  getHealt(){
+  getHealt() {
     return this.healt;
   }
 
-  getPosition(){
+  getPosition() {
     return this.position;
   }
 }
 
 class Game {
-  private level: number;
-  private healt: number;
-  private position: string;
+  private level: number = 1;
+  private healt: number = 100;
+  private position: string = 'Inicio';
 
-  constructor(level: number, healt: number, position: string){
-    this.level = level;
-    this.healt = healt;
-    this.position = position;
-    console.log(`Juando en el nivel ${ this.level }
-                  Salud: ${ this.healt }
-                  posicion ${ this.position }
+  constructor() {
+    console.log(`Juando en el nivel ${this.level}
+                  Salud: ${this.healt}
+                  posicion ${this.position}
                   `);
   }
 
   save(): GameMemento {
-    return new GameMemento(this.level,this.healt,this.position);
+    return new GameMemento(this.level, this.healt, this.position);
   }
 
-  play(level: number, healt: number, position: string):void {
+  play(level: number, healt: number, position: string): void {
     this.level = level;
     this.healt = healt;
     this.position = position;
 
-    console.log(`Jugando en el nivel ${ this.level } salud: ${ this.healt } posicion: ${ this.position }`);
+    console.log(`Jugando en el nivel ${this.level} salud: ${this.healt} posicion: ${this.position}`);
   }
 
   restore(memento: GameMemento): void {
@@ -71,14 +68,14 @@ class Game {
         salud: ${this.healt}
         posicíon: ${this.position}  
       `, COLORS.yellow, COLORS.blue, COLORS.white);
-    
+
   }
 }
 
 class GameHistory {
   private mementos: GameMemento[] = [];
 
-  push(memento: GameMemento){
+  push(memento: GameMemento) {
     return this.mementos.push(memento);
   }
 
@@ -87,3 +84,26 @@ class GameHistory {
   }
 }
 
+function main(){
+  const game = new Game();
+  const history = new GameHistory();
+
+  game.play(2,90,'Bosque Encantadp');
+  history.push(game.save());
+
+  game.play(3,70,'Cuenta Oscura');
+  history.push(game.save());
+
+  game.play(4,50,'Castillo del Dragón');
+  console.log('%c\nEstado Actual', COLORS.green);
+
+  game.restore(history.pop()!);
+  console.log(
+    '%c\nDespues de restaurar el ultimo estado guardado',
+    COLORS.green
+  );
+  
+  console.log('\n\n');
+}
+
+main();
